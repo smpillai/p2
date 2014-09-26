84a411697453e1661ed740d3603a5630cb52d93a
smpillai <spillai01@g.harvard.edu>
2014-09-25 20:56:01 -0400

    Added p2 file and logic.php filewhich contains some sript for the password generator

---
+++ p2.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Second Project: XKCD Password generator</title>
+	
+		
+</head>
+<body>
+
+<?php include 'logic.php';?>
+	
+	
+</body>
+</html>
